7f714560bcdefd54ca1d696c5273ebc60d76f091
amin <m.rezasoltani92@gmail.com>
2017-11-13 09:57:14 +0330

    index.html deleted

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
   <body>
     <h1>Welcome to Index Page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, incidunt placeat.</p>
-    <p>paragraph test2</p>
-    <p>paragraph test3</p>
     <script src="js/main.js"></script>
   </body>
 </html>
