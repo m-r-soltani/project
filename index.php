74e1541d9476a5753ec83fbc48f6918de33bec76
amin <a.rezasoltani92@gmail.com>
2017-11-18 16:52:08 +0330

    add ul to index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
   <body>
     <h1>Welcome to Index Page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, incidunt placeat.</p>
+    <ul>
+        <li>Rocket.ir</li>
+    </ul>
     <script src="js/main.js"></script>
   </body>
 </html>
